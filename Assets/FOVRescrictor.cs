298c2d7be2ce245571e38bb7297c469e1f148ecf
Chen <whitebai12221@gmail.com>
2020-03-16 00:53:12 -0700

    more

---
+++ Assets/FOVRescrictor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FOVRescrictor : MonoBehaviour
+{
+    private Camera head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera head = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
