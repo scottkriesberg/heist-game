3b615cee11077be2c984d754bdaedcd560c36edf
Chen <whitebai12221@gmail.com>
2020-03-17 19:22:06 -0700

    more

--- Assets/FOVRescrictor.cs
+++ Assets/FOVRescrictor.cs
@@ -1,17 +1,38 @@
 ﻿using UnityEngine;
 
 public class FOVRescrictor : MonoBehaviour
-{
+{   
+    [SerializeField]
+    private Vector3 defaultSize;
+    [SerializeField]
+    private Vector3 maxSize;
+    [SerializeField]
+    private Vector3 minSize;
+
     private Camera head;
+    private Vector3 oldPos;
+    private Vector3 curPos;
+    private GameObject restrictor;
     // Start is called before the first frame update
     void Start()
     {
-        Camera head = GetComponent<Camera>();
+        head = GetComponent<Camera>();
+        restrictor = this.gameObject;
+        restrictor.transform.localScale = defaultSize;
+        oldPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        curPos = transform.position;
+        Vector3 velocity = (curPos - oldPos) / Time.deltaTime;
+        Debug.Log("velocity = " + velocity);
+        oldPos = curPos;
+    }
+    
+    private void AdjustRestrictor()
+    {
+
     }
 }
