cf46b23a3ecc25ae646095f0be6210bf350cab67
Chen <whitebai12221@gmail.com>
2020-04-08 18:14:41 -0700

    vig finished

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,25 @@ using Valve.VR.InteractionSystem;
 
 public class GameManager : MonoBehaviour
 {
+    // For FOV
+    [SerializeField]
+    private float fovChangeRate = 3.0f;
+    [SerializeField]
+    private float maxSpeed = 1.2f;
+    [SerializeField]
+    private float minSpeed = 0.8f;
+    [SerializeField]
+    private float maxFOV = 0.8f;
+    [SerializeField]
+    private float minFOV = 0.3f;
+    [SerializeField]
+    private PostProcessProfile fovProfile;
+    private Vector3 oldPos;
+    private Vector3 curPos;
+    private Camera camera;
+    private PostProcessVolume fovVolume;
+
+    // .........................
     public static GameManager Instance;
     public static int laserLayer;
 
@@ -32,6 +51,10 @@ public class GameManager : MonoBehaviour
 
         SceneManager.sceneLoaded += (a, b) => this.SetCameraMode(false);
     }
+    void Update()
+    {
+        FOVRestrict();
+    }
 
     public void RegisterSpawnPoint(PlayerSpawnPoint point)
     {
@@ -43,6 +66,11 @@ public class GameManager : MonoBehaviour
 
         this.currPlayer.position = point.transform.position;
         this.currPlayer.rotation = point.transform.rotation;
+
+        // find the camera
+        this.camera = currPlayer.GetComponentInChildren<Camera>();
+        this.fovVolume = currPlayer.GetComponentInChildren<PostProcessVolume>();
+        fovVolume.isGlobal = true;
     }
 
     public void SetCameraMode(bool infrared)
@@ -56,5 +84,27 @@ public class GameManager : MonoBehaviour
             if (infrared) cam.cullingMask = cam.cullingMask | (1 << laserLayer);
             else cam.cullingMask = cam.cullingMask & (~0 ^ (1 << laserLayer));
         }
+    }
+
+    void FOVRestrict()
+    {
+        Vignette FOV = null;
+        this.fovProfile.TryGetSettings<Vignette>(out FOV);
+        AdjustRestrictor(FOV);
+    }
+
+    private void AdjustRestrictor(Vignette FOV)
+    {
+        curPos = camera.transform.position;
+        Vector3 velocity = (curPos - oldPos) / Time.deltaTime;
+        oldPos = curPos;
+        float exFOV = maxFOV;
+        float curSpeed = velocity.magnitude;
+        if (curSpeed < maxSpeed)
+        {
+            Debug.Log("curSpeed = " + curSpeed);
+            exFOV = Mathf.Max((velocity.magnitude * fovChangeRate / maxSpeed) * maxFOV, this.minFOV);
+        }
+        FOV.intensity.value = Mathf.Lerp(FOV.intensity.value, exFOV, 0.005f);
     }
 }
---
+++ Assets/controlVig.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine.Rendering.PostProcessing;
+using UnityEngine;
+
+public class controlVig : MonoBehaviour
+{
+    [SerializeField]
+    private float FOVChangeRate = 1.0f;
+    [SerializeField]
+    Camera camera;
+    [SerializeField]
+    private float maxSpeed = 1.2f;
+    [SerializeField]
+    private float minSpeed = 0.8f;
+    [SerializeField]
+    private float maxFOV = 0.8f;
+    [SerializeField]
+    private PostProcessProfile camProfile;
+    private Vector3 oldPos;
+    private Vector3 curPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldPos = camera.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FOVRestrict();
+    }
+
+    void FOVRestrict()
+    {
+        Vignette FOV = null;
+        this.camProfile.TryGetSettings<Vignette>(out FOV);
+        AdjustRestrictor(FOV);
+    }
+
+    private void AdjustRestrictor(Vignette FOV)
+    {
+        curPos = camera.transform.position;
+        Vector3 velocity = (curPos - oldPos) / Time.deltaTime;
+        oldPos = curPos;
+        float exFOV = maxFOV;
+        float curSpeed = velocity.magnitude;
+        if (curSpeed < maxSpeed)
+        {
+            Debug.Log("curSpeed = " + curSpeed);
+            
+            exFOV = (velocity.magnitude * FOVChangeRate / maxSpeed) * maxFOV;
+            // Debug.Log(e)
+        }
+        FOV.intensity.value = Mathf.Lerp(FOV.intensity.value, exFOV, 0.005f);
+    }
+}
