335d3611d5e4d3e8e3210774282d064a4448141b
Chen <whitebai12221@gmail.com>
2020-04-14 22:14:40 -0700

    guard and sky

---
+++ Assets/Scenes/GuardModel/GuardAnimController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardAnimController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+
+    private float cur;
+    private float prev;
+    private bool walk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        walk = true;
+        prev = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cur = Time.time;
+        if (cur - prev >= 5)
+        {
+            Debug.Log(walk);
+            walk = !walk;
+            prev = cur;
+        }
+        anim.SetBool("isWalking", walk);
+    }
+}
